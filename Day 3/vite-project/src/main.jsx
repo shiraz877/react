1576d4416fdc2f70f922c4965418e35adbe61f8f
Mohammad Shiraz <shiraz@sofyrus.com>
2026-04-13 16:31:57 +0000

    counter app

--- Day 3/vite-project/src/main.jsx
+++ Day 3/vite-project/src/main.jsx
@@ -1,9 +1,8 @@
 import { createRoot } from "react-dom/client";
-import App, { Shiraz } from "./App";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <>
     <App />
-    <Shiraz />
   </>,
 );
