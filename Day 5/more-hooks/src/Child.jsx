15a9a026584085f6ad807d07bfc4ef3275fec562
Mohammad Shiraz <shiraz@sofyrus.com>
2026-04-15 18:17:18 +0000

    useEffect implemented

---
+++ Day 5/more-hooks/src/Child.jsx
@@ -0,0 +1,15 @@
+// const Child=React.memo(function Child({name}){
+//      console.log("hi");
+   
+//     return(
+//         <h2>Hello {name}</h2>
+//     )
+// })
+function Child({name}){
+   console.log("hi");
+   
+    return(
+        <h2>Hello {name}</h2>
+    )
+}
+export default Child
